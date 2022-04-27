b96ec08682939f56c4ac9970c66b77bcc69e33c5
Julian Boada <julian.boadaortiz@gmail.com>
2022-04-26 22:56:30 -0300

    usuario y contraseña

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 
 // usuario y contraseña
+
+
+function saludo(persona){
+    alert(`Hola ${persona} vienvenido a `)
+}
+
+const pedirNombre =()=> {
+    let nombre = prompt("Dime tu nombre")
+    saluda(nombre)
+}
+
+
 const usuarioContrasena = () =>{
     let usuario = prompt("Ingrese su Usuario")
     while ( usuario != "julian"){
     alert( "Usuario Incorrecto");
     usuario = prompt("Ingrese  su usuario")
-
     return usuario
 } 
     let contrasena = prompt("Ingrese la contraeña")
@@ -13,6 +24,16 @@ const usuarioContrasena = () =>{
     alert("Contraseña incorresta")
     contrasena = prompt("Ingrese la contraeña")
 }   
-    alert (`Hola ${usuario}, Vienvenido a Gomelski Griil & Market`)
+
+    alert (`Hola ${usuario}, Bienvenido a Gomelski Griil & Market`)
 }
+const saluda=(usuario)=>{
+    alert(`Bienvenido${usuario}`)
+    
+}
+pedirNombre()
 usuarioContrasena()
+saluda()
+ 
+
+
